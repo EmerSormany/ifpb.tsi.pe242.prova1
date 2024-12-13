b1e73e12aa3dd2f71885e8fe9b5ee09fd88af3f1
Emerson Sormany <sormanyemerson@gmail.com>
2024-12-13 17:40:10 +0000

    resolução completa

--- main.py
+++ main.py
@@ -68,3 +68,35 @@ def q2():
     print(value)
     
 
+def q3():
+    count = 0
+    number = int(input('digite '))
+    for x in range(1, number+1):
+        if number % x == 0:
+            if x % 3 == 0:
+                count+=1
+    
+    if count == 0:
+        print('O número não possui divisores multiplos de 3')
+    else:
+        print('Quantidade de divisores divisiveis por 3:', count)
+
+def q4():
+    a = int(input(""))
+    b = int(input(""))
+
+    count = 0
+    if a < 0 and b < 0:
+        print('Não há números postivos no intervalo')
+    elif a < 0 and b > 0:
+        for x in range(0, b+1):
+            count+=x
+    elif b < 0 and a > 0:
+        for x in range(0, a+1):
+            count+=x
+    else:
+        for x in range(a, b+1):
+            count+=x
+    print(count)
+
+    
--- q.py
+++ q.py
@@ -1,62 +1,50 @@
+# Escreva um programa que receba como entrada dois números inteiros e retorne a soma dos números positivos no intervalo definido por eles, considerando inclusive os extremos.
+# Referência
+# Obs: o intervalo pode ser crescente ou decrescente
+# ### Exemplos
+# #### Exemplo 1:
+# Entrada:
+# -2
+# 2
+# Saída:
+# 3
+# #### Exemplo 2:
+# Entrada:
+# 1
+# 3
+# Saída:
+# 3
+# #### Exemplo 3:
+# Entrada:
+# 10
+# -10
+# Saída:
+# 55
+# Formato de entrada
+# Dois números inteiros
+# Dica: os números podem ser informados em qualquer ordem (não necessariamente o primeiro será menor que o segundo)
+# Formato de saída
+# Um número inteiro
 
+def q4(a, b):
 
-def q2():
-    """
-    Dado um numeral romano, converta-o para um número inteiro.
-    I	1 feito
-    V	5 feito
-    X	10 feito
-    L	50
-    C	100
-    D	500
-    M	1000
-    """
-    roman = input("digite")
-    value = 0
+    # a = int(input("digite "))
+    # b = int(input("digite "))
 
-    inicio = 0
-    fim = 2
-
-    for i in roman:
-        if i == 'I':
-            if roman[inicio:fim] == 'IV':
-                value += 4
-                value -= 5
-            elif roman[inicio:fim] == 'IX':
-                value += 9
-                value -= 10
-            else:
-                value += 1
-        elif i == 'V':
-            value += 5
-        elif i == 'X':
-            if roman[inicio:fim] == 'XL':
-                value += 40
-                value -= 50
-            elif roman[inicio:fim] == 'XC':
-                value += 90
-                value -= 100
-            else:
-                value += 10
-        elif i == 'L':
-            value += 50
-        elif i == 'C':
-            if roman[inicio:fim] == 'CD':
-                value += 400
-                value -= 500
-            elif roman[inicio:fim] == 'CM':
-                value += 900
-                value -= 1000
-            else:
-                value += 100
-        elif i == 'D':
-            value += 500
-        elif i == 'M':
-            value += 1000 
-
-        inicio+=1
-        fim+=1
-    print(value)
-
-q2()
-
+    count = 0
+    if a < 0 and b < 0:
+        print('Não há números postivos no intervalo')
+    elif a < 0 and b > 0:
+        for x in range(0, b+1):
+            count+=x
+    elif b < 0 and a > 0:
+        for x in range(0, a+1):
+            count+=x
+    else:
+        for x in range(a, b+1):
+            count+=x
+    print(count)
+        
+q4(1,3)
+q4(-2,2)
+q4(10,-10)
